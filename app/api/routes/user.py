d4667c8f1dd3e3226fe8fbbc9b52753ae0a11b75
Q219057 <Q219057@DESKTOP-LLJDU3K>
2025-12-22 17:07:46 +0900

    user api 오류수정

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -1,6 +1,6 @@
 
 
-from typing import Any
+from typing import Any, List
 import uuid
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import select
@@ -11,38 +11,6 @@ from app.models.base import Message
 from app.models.user import PasswordUpdate, User, UserCreate, UserRegister, UserResponse, UserUpdate
 
 router = APIRouter(prefix="/users", tags=["users"])
-
-# user
-@router.get("/", response_model=UserResponse, dependencies=[Depends(get_current_active_superuser)])
-def get_user(session: SessionDep) -> Any:
-    """사용자 리스트 조회"""
-    return session.exec(select(User)).all()
-
-@router.get("/{user_id}", response_model=UserResponse)
-def get_user(session: SessionDep, user_id:uuid.UUID, current_user: CurrentUser) -> Any:
-    """사용자 조회"""
-    db_user = session.get(User,user_id)
-    if db_user.id != current_user.id and not current_user.is_superuser:
-        raise HTTPException(status_code=403, detail="not Authorized")
-    return db_user
-
-@router.patch("/{user_id}", response_model=UserResponse, dependencies=[Depends(get_current_active_superuser)])
-def update_user(session: SessionDep, user_id: uuid.UUID, user_in: UserUpdate):
-    """사용자 수정"""
-    db_user = session.get(User, user_id)
-    user_data = user_in.model_dump(exclude_unset=True)
-    db_user.sqlmodel_update(user_data)
-    session.commit()
-    session.refresh(db_user)
-    return db_user
-
-@router.delete("/{user_id}", dependencies=[Depends(get_current_active_superuser)])
-def delete_user(session: SessionDep, current_user: CurrentUser, user_id: uuid.UUID):
-    """사용자 삭제"""
-    db_user = session.get(User,user_id)
-    session.delete(db_user)
-    session.commit()
-    return Message(message="User deleted successfully")
 
 # me
 @router.get("/me",response_model=UserResponse)
@@ -97,3 +65,35 @@ def register_user(user_in: UserRegister, session: SessionDep) -> Any:
     session.commit()
     session.refresh(db_obj)
     return db_obj
+
+# user
+@router.get("/", response_model=List[UserResponse], dependencies=[Depends(get_current_active_superuser)])
+def get_user(session: SessionDep, skip: int=0, limit: int=100) -> Any:
+    """사용자 리스트 조회"""
+    return session.exec(select(User).offset(skip).limit(limit)).all()
+
+@router.get("/{user_id}", response_model=UserResponse)
+def get_user(session: SessionDep, user_id:uuid.UUID, current_user: CurrentUser) -> Any:
+    """사용자 조회"""
+    db_user = session.get(User,user_id)
+    if db_user.id != current_user.id and not current_user.is_superuser:
+        raise HTTPException(status_code=403, detail="not Authorized")
+    return db_user
+
+@router.patch("/{user_id}", response_model=UserResponse, dependencies=[Depends(get_current_active_superuser)])
+def update_user(session: SessionDep, user_id: uuid.UUID, user_in: UserUpdate):
+    """사용자 수정"""
+    db_user = session.get(User, user_id)
+    user_data = user_in.model_dump(exclude_unset=True)
+    db_user.sqlmodel_update(user_data)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+@router.delete("/{user_id}", dependencies=[Depends(get_current_active_superuser)])
+def delete_user(session: SessionDep, current_user: CurrentUser, user_id: uuid.UUID):
+    """사용자 삭제"""
+    db_user = session.get(User,user_id)
+    session.delete(db_user)
+    session.commit()
+    return Message(message="User deleted successfully")
